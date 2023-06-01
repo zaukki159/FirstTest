ef25eebc5afd5dafed2d7e8b1f8ac1b3d64de9e1
zaukki159 <miikka-97@hotmail.com>
2023-06-01 21:55:48 +0300

    commit_010623
    Making main menu and rotation for player and enemyes

---
+++ Assets/enemySpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float timeToSpawn = 10f;
+    [SerializeField]
+    private float current_Timer = 0f;
+
+    public GameObject enemyFighter;
+    public Transform spawnPosition;
+
+    private void Update()
+    {
+        current_Timer += Time.deltaTime;
+
+        if(current_Timer>timeToSpawn)
+        {
+            Instantiate(enemyFighter, spawnPosition.position+new Vector3(Random.Range(-21,26),0), Quaternion.identity);
+            current_Timer = 0;
+        }
+    }
+
+}
